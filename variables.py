042a92881b3ab470558ab2cc56936f9902be2ee1
JavierPulidoT <javierp.pulido@gmail.com>
2024-01-02 22:46:41 -0600

    variables

---
+++ variables.py
@@ -0,0 +1,33 @@
+a = 2   #int
+b = 3.2  #float
+c = a + b
+x = True   #boolean
+print(c)
+
+#Definiendo variable con camelCase
+nombreCompleto = "Javier Pulido"
+
+
+#Definiendo variable con snake_case... recomendado para python
+nombre_completo_1 = "Javier Pulido"
+
+#Concatenar com +
+n = 'Javier'
+bienvenida = "Hola " + n + " Como estas?"
+print(bienvenida)
+
+#Concatenar con f-string
+#todo lo que este en f{} lo convierte en texto
+n2 = 'Javier'
+bienvenida2 = f"Hola  {n2}  Como estas?"
+
+#Dejar declarar una variable
+#del bienvenida2
+
+print(bienvenida2)
+
+#Operadores de pertenencia (in / not in) 
+print("Hola" in bienvenida2) #true
+print("Hola" not in bienvenida2) #false
+
+#Operadores de identidad
