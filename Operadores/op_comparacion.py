a5df87bad8dcf76a8e0a7d41289be0e4ff604952
JavierPulidoT <javierp.pulido@gmail.com>
2024-01-05 22:34:33 -0600

    Operadores comparacion

---
+++ Operadores/op_comparacion.py
@@ -0,0 +1,31 @@
+#Operadores de comparacion
+
+igual_que = 5 == 6
+
+distinto_de = 5!= 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 5
+
+#mostrara si es True or False
+print(mayor_o_igual) 
+
+#Calculos combinados
+
+a = 5
+b = 10
+c = 20 
+comparacion = a + b == c
+
+print(comparacion)
+
+#comparar 
+pass_almacenada ="1234"
+pass_escrita = "1234"
+
+print(pass_almacenada == pass_escrita)
