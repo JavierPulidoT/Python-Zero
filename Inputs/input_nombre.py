479a11ac1536ddec18fc6cbc481096410bedda0d
JavierPulidoT <javierp.pulido@gmail.com>
2024-01-09 21:18:24 -0600

    Inputs

---
+++ Inputs/input_nombre.py
@@ -0,0 +1,7 @@
+#Pedirle un dato al usuario
+
+nombre = input("Dame tu nombre: ")
+
+#mostrando nombre
+print(f'El nombre es: {nombre}')
+
---
+++ Inputs/input_numeros.py
@@ -0,0 +1,8 @@
+#pedir Numero
+
+numero = input("Ingresa Numero: ")
+
+#Multiplicando x 2 el numero
+resultado = int(numero) * 2  #Convierto el numero a int
+
+print(f'El resultado es {resultado}')
