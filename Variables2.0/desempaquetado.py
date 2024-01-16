011d9f630fe030bc93f6064db35c5d2f025449e9
JavierPulidoT <javierp.pulido@gmail.com>
2024-01-15 23:21:18 -0600

    Desempaquetado

---
+++ Variables2.0/desempaquetado.py
@@ -0,0 +1,11 @@
+#Desempaquetado
+#Cantidad de variables , debe ser igual a la cantidad de datos del arrays..
+
+#Creando Tupla/lista/conjunto...
+datos_tupla = ("Javier","Pulido",10)
+datos_lista = ["Javier","Pulido",10]
+
+#Desempaquetado
+nombre,apellido,numero = datos_tupla
+
+print(nombre)
