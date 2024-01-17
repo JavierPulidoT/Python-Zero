64262c7ab3a958c41b9e8a1d78680978aab816f8
JavierPulidoT <javierp.pulido@gmail.com>
2024-01-16 21:43:23 -0600

    Tuplas

---
+++ Variables2.0/tuplas.py
@@ -0,0 +1,15 @@
+#Crar Tuplas con tuple()
+tupla = tuple(["dato","dato2"])
+
+#tupla mas sencilla... sin ()
+tupla = "dato1","dato2"
+
+#tupla con un solo dato
+tupla = "dato1",
+
+print(tupla)
+print(type(tupla))
+
+#las tuplas son ideales para datos de solo lectura
+
+#Las listas se usan para datos mas flexibles
