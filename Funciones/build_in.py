c85b8ed47fb88f8e22a5a9a0b94f417e4ff53a57
JavierPulidoT <javierp.pulido@gmail.com>
2024-02-01 23:58:14 -0600

    build in - max -min

---
+++ Funciones/build_in.py
@@ -0,0 +1,12 @@
+
+numeros = [4,7,8,6,55,23]
+
+#Encontrando el numero mayor para una lista
+
+numero_mas_alto = max(numeros)
+print(numero_mas_alto)
+
+#Encontrando el numero menor para una lista
+
+numero_mas_bajo = min(numeros)
+print(numero_mas_bajo)
